f16e19b202fa8a1f600687ca093569cd10aa32a0
mohamed.jalloh <mohamed.jalloh@WEPLUS97.wenet.local>
2022-02-18 09:29:58 +0100

    Loops

---
+++ Revision/src/WhileDemo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class WhileDemo {
+	
+	public static void main(String [] args) {
+		
+		
+		int count, number;
+		System.out.println("Enter a number");
+		Scanner Keyboard= new Scanner(System.in);
+		number=Keyboard.nextInt();
+		count=1;
+		
+		while (count <=number) {
+			System.out.print(count+", ");
+			count++;
+			
+		}
+		System.out.println();
+		System.out.println("Buckle my shoe.");
+	}
+
+}
